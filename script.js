31c71cf714f955720d563236f636f2c1abdf40b1
ryomen-sukuna-alt <normamatov.aziz0720@gmail.com>
2024-10-25 13:47:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let a = prompt('string typidagi malutni kiriting')
+let b = +prompt('number typidagi malutni kiriting')
+let c = prompt('boolean typidagi malutni kiriting')
+alert('siz kiritgan malumot uchun rahmat (❤´艸｀❤)')
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
